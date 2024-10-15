582d39c77d587dd858d2372573a3fa45ee6f4e77
Shanjidul Hasan Shajid <shanjidulhasan.s@gmail.com>
2024-10-15 16:45:18 +0600

    update 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import { FiMenu } from "react-icons/fi";
 import { LuGlobe } from "react-icons/lu";
 import { FaUserCircle } from "react-icons/fa";
 import Navbar from "./components/navbar";
+import SearchBar from "./components/searchBar";
 import "./App.css";
 
 function App() {
-  return <Navbar />;
+  return (<>
+    <Navbar />
+    <SearchBar/>
+    </>
+  );
+
 }
 
 export default App;
---
+++ src/components/searchBar.jsx
@@ -0,0 +1,9 @@
+import "./searchBar.css";
+
+export default function SearchBar(){
+    return (
+        <div className="nega">
+            <p>search bar</p>
+        </div>
+    )
+}
